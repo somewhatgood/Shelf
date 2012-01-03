28bac15147bc37e9cec9d50ac53e925ff8b8eb6b
somewhatgood <somewhatgood@Hawaii.local>
2012-01-03 19:48:43 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shelf::Application.config.secret_token = 'a859803e2ed1b2ecb921138e1dc26e1dd423c8ae9259000ab4483f3ec09ff6663caf4be1ffbd8ab846cb564e72232d1596d5c28e5668a7910882d866c5c6d172'
